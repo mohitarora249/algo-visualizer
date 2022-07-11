e413e08a1689a90005cd632bff521befa83b2c26
mohitarora249 <mohitarora249@gmail.com>
2022-07-11 21:06:14 +0530

    added algo selection

--- src/App.js
+++ src/App.js
@@ -9,43 +9,69 @@ function App() {
     85, 100, 49, 75, 69,
   ];
   const [steps, setSteps] = useState([]);
-  const [current, setCurrent] = useState([]);
-  useEffect(() => {
-    setSteps(bubbleSort([...input]));
-  }, []);
+  const [currentStep, setCurrentStep] = useState(input);
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState("");
+  const [intervalId, setIntervalId] = useState(null);
+
+  const ALGOS = [
+    { name: "Bubble Sort", algo: bubbleSort },
+    { name: "Selection Sort", algo: selectionSort },
+    { name: "Insertion Sort", algo: insertionSort },
+  ];
 
   useEffect(() => {
-    let interval = null;
     if (steps.length) {
       let i = 0;
-      interval = setInterval(() => {
+      const interval = setInterval(() => {
         if (i === steps.length - 1) {
-          clearInterval(interval);
+          clearInterval(intervalId);
         }
-        setCurrent(steps[i]);
+        setCurrentStep(steps[i]);
         i++;
       }, 100);
+      setIntervalId(interval);
     }
     return () => {
-      clearInterval(interval);
+      clearInterval(intervalId);
     };
   }, [steps]);
 
+  const algoSelectHdlr = (e) => {
+    setSteps([]);
+    clearInterval(intervalId);
+    setSelectedAlgorithm(e.target.id);
+    const algorithm = ALGOS.find((a) => a.name === e.target.id).algo;
+    setSteps(algorithm(input));
+  };
+
   return (
     <div className="h-screen w-screen flex items-center flex-col bg-black text-[#77898B]">
-      <div className="mt-12 mb-6 text-md font-bold text-4xl">
-        INSERTION SORT
+      <div className="mt-12 mb-6 text-4xl font-bold flex w-full justify-around">
+        {ALGOS.map((algo) => (
+          <div
+            key={algo.name}
+            id={algo.name}
+            onClick={algoSelectHdlr}
+            className={
+              selectedAlgorithm === algo.name
+                ? "border-b-4 border-[#77898B]"
+                : ""
+            }
+          >
+            {algo.name}
+          </div>
+        ))}
       </div>
       <div className="flex">
-        {current.map((x, idx) => (
+        {currentStep.map((step, idx) => (
           <>
             <div
               className="w-12 relative border border-black bg-[#77898B] mt-auto text-sm rounded-t-sm"
-              style={{ height: x * 7 }}
+              style={{ height: step * 7 }}
               key={idx}
             >
               <span className="text-xl font-semibold absolute bottom-[-40px]">
-                {x}
+                {step}
               </span>
             </div>
           </>
